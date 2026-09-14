638e9ab2c3ce8e4ae86812da65f980e9728e534d
hewittcoleprep <hewitt.cole@hermits.org>
2026-09-14 18:33:22 +0000

    Adds user input and makes main use variables instead of placeholder

--- src/calculator.py
+++ src/calculator.py
@@ -1,8 +1,22 @@
+import numbers
 """ATCS Unit 0 demonstration calculator.
 
 This program is intentionally simple so students can focus on
 professional software-engineering workflow rather than syntax.
 """
+a = ""
+b = ""
+while not isinstance(a, numbers.Number):
+    try:
+        a = float(input("Input a number for A: "))
+    except ValueError:
+        continue
+
+while not isinstance(b, numbers.Number):
+    try:
+        b = float(input("Input a number for B: "))
+    except ValueError:
+        continue
 
 
 def add(a, b):
@@ -17,8 +31,8 @@ def subtract(a, b):
 
 def main():
     print("Engineering Calculator")
-    print("5 + 3 =", add(5, 3))
-    print("5 - 3 =", subtract(5, 3))
+    print(a," + ",b," =", add(a, b))
+    print(a," + ",b," =", subtract(a, b))
 
 
 if __name__ == "__main__":
